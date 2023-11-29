22a059d1beee25dde986b3330e9d20a2d8b64614
sagar samvedi <sagarsamvedi31@gmail.com>
2023-11-29 14:07:19 +0530

    Added search bar functionality

--- scripts/index.js
+++ scripts/index.js
@@ -53,3 +53,31 @@ function displayItemsOnHomePage(){
 itemsContainerElement.innerHTML = innerHtml
 }
 
+
+function searchResult() {
+    let searchQuery = document.querySelector('.search_input').value.toLowerCase(); // Convert to lowercase
+    let searchArray = items.filter(item => item.item_name.toLowerCase().includes(searchQuery));
+    let itemsContainerElement = document.querySelector('.items-container');
+    if (!itemsContainerElement) {
+        return;
+    }
+    let innerHtml = '';
+
+    searchArray.forEach(item => {
+        innerHtml += `<div class="item-container">
+        <img class= 'item-image' src="${item.image}">
+        <div class="rating">
+        ${item.rating.stars} ⭐ | ${item.rating.count}
+        </div>
+        <div class="company-name">${item.company}</div>
+        <div class="item-name">${item.item_name}</div>
+        <div class="price">
+            <span class="current-price">₹ ${item.current_price}</span>
+            <span class="original-price">₹ ${item.original_price}</span>
+            <span class="discount">(${item.discount_percentage} %  OFF)</span>
+        </div>
+        <button class="btn-add-bag" onclick='addToBag(${item.id})'>Add to Bag</button>
+        </div>`;
+    });
+    itemsContainerElement.innerHTML = innerHtml;
+}
